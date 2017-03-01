9b40b8b151e9a1e83c8973fc28cbbdf1a94a7a31
Ashwani Kumar <ashwani.k@tanglesolutions.com>
2017-03-01 12:29:56 +0530

    time.js added

---
+++ time.js
@@ -0,0 +1,20 @@
+/*two dates comparison*/
+
+var date_start = $dialogContent.find("input[id='edit-start-date--2-datepicker-popup-0']").val();
+var date_end = $dialogContent.find("input[id='edit-end-date-datepicker-popup-0']").val();
+var startField = $dialogContent.find("input[id='edit-start-date--2-timeEntry-popup-1']").val();
+var endField = $dialogContent.find("input[id='edit-end-date-timeEntry-popup-1']").val();
+var validate = true;
+var end = parseInt(endField);
+var start = parseInt(startField);
+
+var d1 = new Date(date_start);
+var d2 = new Date(date_end);
+
+                                console.log("----------");   // prints false (wrong!)
+                                console.log(d1 == d2);   // prints false (wrong!)
+                                console.log(d1 === d2);  // prints false (wrong!)
+                                console.log(d1 != d2);   // prints true  (wrong!)
+                                console.log(d1 !== d2);  // prints true  (wrong!)
+                                console.log(d1.getTime() === d2.getTime()); // prints true (correct)
+                                console.log(d1 > d2);
